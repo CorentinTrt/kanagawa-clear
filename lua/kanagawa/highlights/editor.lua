0de1371da2cdde3e2bc1fc91d0144ee9f5ac1911
CorentinTrt <corentintruffaut@gmail.com>
2024-12-04 17:58:32 -0500

    feat(colors): remove status line bg

--- lua/kanagawa/highlights/editor.lua
+++ lua/kanagawa/highlights/editor.lua
@@ -118,13 +118,13 @@ function M.setup(colors, config)
         -- SpellRare	Word that is recognized by the spellchecker as one that is hardly ever used. |spell| Combined with the highlighting used otherwise.
         SpellRare = { undercurl = config.undercurl, underline = not config.undercurl, sp = theme.diag.warning },
         -- StatusLine	Status line of current window.
-        StatusLine = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        StatusLine = { fg = theme.ui.fg_dim, bg = "NONE" },
         -- StatusLineNC	Status lines of not-current windows. Note: If this is equal to "StatusLine", Vim will use "^^^" in the status line of the current window.
         StatusLineNC = { fg = theme.ui.nontext, bg = theme.ui.bg_m3 },
         -- TabLine		Tab pages line, not active tab page label.
-        TabLine = { bg = "NONE", fg = theme.ui.special },
+        TabLine = { bg = theme.ui.bg_m3, fg = theme.ui.special },
         -- TabLineFill	Tab pages line, where there are no labels.
-        TabLineFill = { bg = "NONE" },
+        TabLineFill = { bg = theme.ui.bg },
         -- TabLineSel	Tab pages line, active tab page label.
         TabLineSel = { fg = theme.ui.fg_dim, bg = theme.ui.bg_p1 },
         -- Title		Titles for output from ":set all", ":autocmd" etc.
